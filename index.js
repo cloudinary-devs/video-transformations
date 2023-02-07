11c1be189748979fbdf1766e67f96ae637d0aa13
Sharon Yelenik <sharon.yelenik@cloudinary.com>
2023-02-07 20:23:58 +0200

    gravity fix

--- index.js
+++ index.js
@@ -17,22 +17,32 @@ cloudinary.config({
 //////////////////
 (async () => {
 
-    let videoURL = cloudinary.url("docs/video_features_tutorial/hair", {
-        resource_type: "video",
-        transformation: [
-          { aspect_ratio: "3:4", crop: "fill", gravity: "north", width: 250 },
-          { duration: "5"},
-          { duration: "5", flags: "splice", overlay: "video:docs:video_features_tutorial:makeup"},
-          { aspect_ratio: "3:4", crop: "fill", gravity: "north", width: 250, flags: "layer_apply" },
-          { overlay: "video:docs:video_features_tutorial:romeo_and_juliet" },
-          { flags: "layer_apply"},
-          { overlay: "cloudinary_icon"},
-          { width: 40, x:10, y:10 },
-          { flags: "layer_apply", gravity: "north_east"},
-          { overlay: {resource_type: "subtitles", public_id: "docs/video_features_tutorial/captions.srt"}},
-          { flag:"layer_apply" },
-        ],
-      });
-    console.log(videoURL);
+
+  let videoURL = cloudinary.url("docs/video_features_tutorial/hair", {
+      resource_type: "video",
+      transformation: [
+        { aspect_ratio: "3:4", crop: "fill", gravity: "north", width: 250 },
+        { duration: "5"},
+        { duration: "5", flags: "splice", overlay: "video:docs:video_features_tutorial:makeup"},
+        { aspect_ratio: "3:4", crop: "fill", gravity: "north", width: 250 }, 
+        { flags: "layer_apply" },
+        { overlay: "video:docs:video_features_tutorial:romeo_and_juliet" },
+        { flags: "layer_apply"},
+        { overlay: "cloudinary_icon"},
+        { width: 40, x:10, y:10 },
+        { flags: "layer_apply", gravity: "north_east"},
+        { overlay: {resource_type: "subtitles", public_id: "docs/video_features_tutorial/captions.srt"}},
+        { flag: "layer_apply" }
+      ],
+    });
+  console.log(videoURL);
+
 
 })();
+
+
+
+
+
+
+
